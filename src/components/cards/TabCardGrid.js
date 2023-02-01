15f0739e7d6dd3f0b15e0d44150baa93408cd088
BoedNoNiwa <bprayoga1927@gmail.com>
2023-02-01 12:55:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/cards/TabCardGrid.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading } from "components/misc/Headings.js";
+import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
+import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-5.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-7.svg";
+import perfumes from '../../json/semerbak.json'
+
+const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
+const Header = tw(SectionHeading)``;
+const TabsControl = tw.div`flex flex-wrap bg-gray-200 px-2 py-2 rounded leading-none mt-12 xl:mt-0`;
+
+const TabControl = styled.div`
+  ${tw`cursor-pointer px-6 py-3 mt-2 sm:mt-0 sm:mr-2 last:mr-0 text-gray-600 font-medium rounded-sm transition duration-300 text-sm sm:text-base w-1/2 sm:w-auto text-center`}
+  &:hover {
+    ${tw`bg-gray-300 text-gray-700`}
+  }
+  ${props => props.active && tw`bg-primary-500! text-gray-100!`}
+  }
+`;
+
+const TabContent = tw(motion.div)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
+const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:pr-10 md:pr-6 lg:pr-12`;
+const Card = tw(motion.a)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+const CardImageContainer = styled.div`
+  ${props => css`background-image: url("${props.imageSrc}");`}
+  ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
+`;
+
+const CardHoverOverlay = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 0.5);
+  ${tw`absolute inset-0 flex justify-center items-center`}
+`;
+const CardButton = tw(PrimaryButtonBase)`text-sm`;
+const CardText = tw.div`p-4 text-gray-900`;
+const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
+const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
+const CardPrice = tw.p`mt-4 text-xl font-bold text-secondary-500`;
+
+const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+  ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-64 w-64 opacity-15 transform translate-x-2/3 -translate-y-12 text-pink-400`}
+`;
+const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
+  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-80 w-80 opacity-15 transform -translate-x-2/3 text-primary-500`}
+`;
+
+export default ({
+  id,
+  heading = "Checkout the Menu",
+  tabs = {
+    Semua: perfumes,
+    Unisex: filterByCategory('unisex'),
+    Pria: filterByCategory('pria'),
+    Wanita: filterByCategory('wanita'),
+  }
+}) => {
+  /*
+   * To customize the tabs, pass in data using the `tabs` prop. It should be an object which contains the name of the tab
+   * as the key and value of the key will be its content (as an array of objects).
+   * To see what attributes are configurable of each object inside this array see the example above for "Starters".
+   */
+  const tabsKeys = Object.keys(tabs);
+  const [activeTab, setActiveTab] = useState(tabsKeys[0]);
+
+  return (
+    <Container id={id}>
+      <ContentWithPaddingXl>
+        <HeaderRow>
+          <Header>{heading}</Header>
+          <TabsControl>
+            {Object.keys(tabs).map((tabName, index) => (
+              <TabControl key={index} active={activeTab === tabName} onClick={() => setActiveTab(tabName)}>
+                {tabName}
+              </TabControl>
+            ))}
+          </TabsControl>
+        </HeaderRow>
+
+        {tabsKeys.map((tabKey, index) => (
+          <TabContent
+            key={index}
+            variants={{
+              current: {
+                opacity: 1,
+                scale:1,
+                display: "flex",
+              },
+              hidden: {
+                opacity: 0,
+                scale:0.8,
+                display: "none",
+              }
+            }}
+            transition={{ duration: 0.4 }}
+            initial={activeTab === tabKey ? "current" : "hidden"}
+            animate={activeTab === tabKey ? "current" : "hidden"}
+          >
+            {tabs[tabKey].map((card, index) => (
+              <CardContainer key={index}>
+                <Card className="group" target={"_blank"} href={card.link} initial="rest" whileHover="hover" animate="rest">
+                  <CardImageContainer imageSrc={card.gambar}>
+                    <CardHoverOverlay
+                      variants={{
+                        hover: {
+                          opacity: 1,
+                          height: "auto"
+                        },
+                        rest: {
+                          opacity: 0,
+                          height: 0
+                        }
+                      }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <CardButton>Beli Sekarang</CardButton>
+                    </CardHoverOverlay>
+                  </CardImageContainer>
+                  <CardText>
+                    <CardTitle>{card.nama}</CardTitle>
+                    <CardContent>{card.kategori.charAt(0).toUpperCase() + card.kategori.slice(1)}</CardContent>
+                    <CardPrice>{card.harga}</CardPrice>
+                  </CardText>
+                </Card>
+              </CardContainer>
+            ))}
+          </TabContent>
+        ))}
+      </ContentWithPaddingXl>
+      <DecoratorBlob1 />
+      <DecoratorBlob2 />
+    </Container>
+  );
+};
+
+const filterByCategory = (category) => {
+  return perfumes.filter((perfume) => perfume.kategori === category)
+}
---
+++ src/demos/RestaurantLandingPage.js
@@ -0,0 +1,105 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+import Header from "../components/headers/light.js";
+import {ReactComponent as ChevronUpIcon} from "../images/chevron-up-icon.svg";
+
+const BackToTopButton = styled.button`
+  ${tw`z-20 focus:outline-none bg-white border-2 border-gray-500/50 rounded-full fixed bottom-0 right-0 mx-5 my-6 p-3 hover:bg-gray-600/50 transition duration-300`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`;
+
+const fragranceIconImageSrc = "images/features/keunggulan-1.png";
+const substanceIconImageSrc = "images/features/keunggulan-2.png"
+const moneyIconImageSrc = "images/features/keunggulan-3.png";
+
+export default () => {
+  const backToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+ 
+  const Subheading = tw.span`tracking-wider text-base font-medium text-primary-400`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
+  return (
+    <>
+      <BackToTopButton onClick={backToTop}><ChevronUpIcon></ChevronUpIcon></BackToTopButton>
+      <AnimationRevealPage>
+        <Header />
+        <MainFeature 
+          id={"tentang-kami"}
+          subheading={<Subheading>Sejak 2017</Subheading>}
+          heading={
+            <>
+              Brand parfum dengan konsentrat asli Eropa
+              <br />
+              <br />  
+              Aroma <HighlightedText>98%</HighlightedText> sama dengan parfum original
+            </>
+          }
+          buttonRounded={false}
+          textOnLeft={false}
+          primaryButtonText="Kunjungi Toko"
+          primaryButtonUrl="https://shopee.co.id/semerbak.parfume"
+          imageSrc={
+            "images/jumbotron/hero.jpg"
+          }
+          imageCss={imageCss}
+          imageDecoratorBlob={true}
+          imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
+        />
+        {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
+        <TabGrid
+          id={"katalog"}
+          heading={
+            <>
+              Katalog <HighlightedText>Semerbak</HighlightedText>
+            </>
+          }
+        />
+        <Features
+          id={"keunggulan"}
+          heading={
+            <>
+              Keunggulan <HighlightedText>Semerbak</HighlightedText>
+            </>
+          }
+          cards={[
+            {
+              imageSrc: fragranceIconImageSrc,
+              title: "Ketahanan Aroma",
+              description: "Aroma parfum tahan hingga 12 sampai 24 Jam",
+            },
+            {
+              imageSrc: substanceIconImageSrc,
+              title: "Bahan Berkualitas",
+              description: "Terbuat dari konsentrat asli Eropa dan alkohol khusus parfum",
+            },
+            {
+              imageSrc: moneyIconImageSrc,
+              title: "Harga Terjangkau",
+              description: "Harga yang ramah dikantong dengan isi yang tahan hingga 1 bulan",
+            }
+          ]}
+
+          imageContainerCss={tw`p-2!`}
+          imageCss={tw`w-20! h-20!`}
+        />
+        <Footer />
+      </AnimationRevealPage>
+    </>
+  );
+}
